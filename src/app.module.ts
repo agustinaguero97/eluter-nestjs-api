d4411b19603a6a85adfa62a1298fd12ed7680533
agustin aguero <aaguero@agustins-MacBook-Pro.local>
2024-06-25 19:06:49 -0300

    configurando swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'eluter_db',
+      username: 'eluter_user',
+      entities: [],
+      database: 'eluter_db',
+      synchronize: true,
+      logging: true,
+    }), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
